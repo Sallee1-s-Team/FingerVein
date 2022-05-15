32a6e1e70687c9d36392208ee7d0dfba8b6b2e6b
Sallee1 <885331635@qq.com>
2022-05-15 11:29:17 +0800

    preprocess

--- Loader.py
+++ Loader.py
@@ -1,16 +1,20 @@
 from torch.utils.data.dataset import Dataset
 from copy import deepcopy
-import math
 import numpy as np
 import torch
 import torch.torch_version 
-from torch import nn
 import cv2
 import csv
 from skimage import morphology
+import shutil
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from matplotlib import pyplot
+#from MyModel import MnistModel
 
-class MnistDataSet(Dataset):
+class FingerVainDataSet(Dataset):
   def __init__(self,rootDir) -> None:
+    self.step = 0
     super().__init__()
     self.FileList = []
     with open(f"{rootDir}/data.csv",mode="r",encoding="utf8") as csvFile:
@@ -20,11 +24,100 @@ class MnistDataSet(Dataset):
         self.FileList.append((f"{rootDir}/{row[0]}_{row[1]}_{row[2]}_{row[3]}_roi.bmp",(int(row[0])-1)*2+int(row[2][1])-1))  #样本编号成数字，其中每两个编号代表一个人的两个不同的手指
 
   def __imgPrepoocess(self,img,target):
+    wd = img.shape[1]
+    ht = img.shape[0]
     # 在这里写上预处理代码
-    
-    img = torch.from_numpy(img)
+    img = (255*(img.astype(float)-img.min()) / (img.max() - img.min())).astype("uint8")
+    #img = self.__getRoi(img,0.8,0.3)      #查找感兴趣区域
+    img = 
+    img = cv2.resize(img,dsize=(300,100),interpolation=cv2.INTER_LINEAR)    #训练需要图像大小相同，统一缩放
+    img = torch.from_numpy(img)           #从numpy转换成张量（用来部署到显卡）
+
+    #return img.float()
     return img
+
+  # 获取裁切区域
+  def __getRoi(self,img:np.ndarray,edgeThread:float,kSizeRate:float):    #图像，边缘亮度钳制，核大小
+    wd = img.shape[1]
+    ht = img.shape[0]
+    cutUp = 0;cutDown=ht;cutLeft=0;cutRight=wd    #裁切点 
+    # 纵向裁切
+    midXPos = wd // 2                             #求中心纵线位置
+    midXHist = img[:,midXPos]                     #获取纵线的亮度变化
+    midXHist = self.__Average1D(midXHist,0.1)     #一维均值滤波
+    midXHist = midXHist.astype(float) / 255       #规格化到0.0~1.0
+    # 输出图像纵向裁切，利用中线值
+    for i in range(ht):                           #从上扫描，遇到低于阈值的点停止
+      if(midXHist[i] < edgeThread): 
+        cutUp = i;break
+    for i in range(ht-1,-1,-1):                   #从下扫描，遇到低于阈值的点停止
+      if(midXHist[i] < edgeThread): 
+        cutDown = i + 1;break
     
+    # 横向裁切
+    referimg = img[15:ht-15,0:wd-50]                     #去除上下和右侧的亮区作为参考图像
+    referimg = cv2.equalizeHist(referimg)                #直方图均衡化
+    referimg = ((referimg.astype(float) / 255) ** 10 * 255).astype("uint8")    #伽马，降低暗区的权重
+    sumYHist = np.sum(referimg,axis=0)                   #获取纵向投影直方图
+    sumYHist = self.__Average1D(sumYHist,0.1)            #一维均值滤波
+    sumYHist = sumYHist.astype(float) / sumYHist.max()   #规格化到0.0~1.0
+
+    # pyplot.plot(sumYHist)
+    # pyplot.show()
+    # 输出图像横向裁切，利用参考投影直方图
+    maxValues = []                                    #查找所有极值点
+    for i in range(1,referimg.shape[1]-1):            #从左扫描，遇到峰值点
+      if(sumYHist[i] >= sumYHist[i-1] and sumYHist[i] >= sumYHist[i+1]):  #找到极值点（或拐点）
+        lUp =False; rUp = False
+        if(sumYHist[i-1]==sumYHist[i+1]):     #如果左右界相同，就需要比较以免选取到拐点
+          for L in range(1,i+1):
+            if(sumYHist[i-L] < sumYHist[i-1]):lUp = True;break;
+            elif(sumYHist[i-L] > sumYHist[i-1]):break;
+          for R in range(1,referimg.shape[1]-i):
+            if(sumYHist[i+R] > sumYHist[i+R]):lUp = True;break;
+            elif(sumYHist[i+R] < sumYHist[i+R]):break;
+          if(not (lUp and rUp)):
+            continue      #是拐点，继续循环
+        maxValues.append(i)
+
+    maxValuesIdx = np.argsort(maxValues)
+    cutLeft = maxValues[maxValuesIdx[0]]
+    cutRight = maxValues[maxValuesIdx[1]]
+
+    if(cutLeft > cutRight):
+      cutLeft,cutRight = cutRight,cutLeft
+    
+    img = img[cutUp:cutDown,cutLeft:cutRight]     #cv2没有裁切函数，需要使用numpy切片实现
+    if(cutUp >= cutDown):print("上下裁切错误")
+    if(cutLeft >= cutRight):print("左右裁切错误")
+    return img
+
+  # 一维均值滤波
+  def __Average1D(self,arr:np.ndarray,windowSize:int)->np.ndarray:
+    arr = arr.astype(float)
+    arrLen = arr.shape[0]
+    cov1dSize = int((windowSize * arrLen) // 2)
+    cov1dres = np.zeros(arr.shape)                            #用来接收一维卷积结果
+    for i in range(-cov1dSize,cov1dSize+1,1):               
+      posX = np.clip(np.arange(i,arrLen+i),0,arrLen-1)        #求坐标数组用来取坐标
+      cov1dres += arr[posX]                                   #卷积累加
+    return cov1dres / (cov1dSize * 2 + 1)                     #计算平均值            
+
+  def __contest(self,img:np.ndarray,pow:float):               #S形对比度增强函数，利用指数/对数函数
+    if(pow == 1):return img
+    mapping = np.arange(0,256)
+    #指数模式
+    if(pow >= 1):
+      Map = np.clip(np.round((np.power(pow,(np.arange(0,128).astype(np.float64))/127)-1)/(pow-1)*127),0,127)
+    #对数模式
+    else:
+      pow = 1 / pow
+      Map = np.clip((np.round((np.log(np.arange(0,128).astype(np.float64)/(127/(pow-1))+1)/np.log(pow))*127)),0,127)
+    mapping[0:128] = Map
+    mapping[255:127:-1] = -Map+255
+    mapping = mapping.astype("uint8")
+    return mapping[img]
+
   def __getitem__(self, index):
     img = cv2.imread(self.FileList[index][0],flags=cv2.IMREAD_GRAYSCALE)
     img = self.__imgPrepoocess(img,self.FileList[index][1])
@@ -34,3 +127,27 @@ class MnistDataSet(Dataset):
       
   def __len__(self):
     return len(self.FileList)
+
+
+if __name__ == '__main__':
+  #参数
+  batch = 200   #批大小
+  #数据
+  trainSet = FingerVainDataSet("Train")
+  trainLoader= DataLoader(trainSet,batch,shuffle=True,num_workers=0,drop_last=True)
+
+  trainLen = len(trainSet)
+
+  shutil.rmtree("TrainLogs",ignore_errors=True)
+  logWriter = SummaryWriter("TrainLogs")
+  for batch in trainLoader:
+    logWriter.add_images("train images",batch[0],1)
+    break
+  logWriter.close()
+  exit(0)
+  
+  trainSet = FingerVainDataSet("Train")
+  shutil.rmtree("TrainLogs",ignore_errors=True)
+  logWriter = SummaryWriter("TrainLogs")
+  logWriter.add_image("testImg",trainSet[0][0])
+  logWriter.close()
--- Train.py
+++ Train.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import torchvision
 import shutil
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.tensorboard import SummaryWriter
@@ -7,21 +8,25 @@ from torch import nn
 from Loader import *
 
 if __name__ == '__main__':
+  #参数
+  learn_rate = 1e-4   #学习率
+  batch = 150   #批大小
+
   #部署GPU
-  #device = torch.device("cuda")
+  device = torch.device("cuda")
   #数据
-  trainSet = MnistDataSet("Train")
-  #verifySet = MnistDataSet("Verify")
-  trainLoader= DataLoader(trainSet,150,shuffle=True,num_workers=0,drop_last=True)
-  #verifyLoader = DataLoader(verifySet,150,shuffle=False)
+  trainSet = FingerVainDataSet("Train")
+  verifySet = FingerVainDataSet("Test")
+  trainLoader= DataLoader(trainSet,batch,shuffle=True,num_workers=0,drop_last=True)
+  verifyLoader = DataLoader(verifySet,batch,shuffle=False)
+
   trainLen = len(trainSet)
-  """
   verifyLen = len(verifySet)
 
   #模型
   #myModel = MnistModel()
   myModel = torchvision.models.vgg16()
-  myModel.classifier[6] = nn.Linear(4096,15)
+  myModel.classifier[6] = nn.Linear(4096,312)
   print(myModel)
   myModel = myModel.to(device)
   
@@ -30,7 +35,6 @@ if __name__ == '__main__':
   lossFn = lossFn.to(device)
 
   #优化器
-  learn_rate = 1e-4
   optimizer = torch.optim.Adam(myModel.parameters(),lr = learn_rate)
   scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,mode="min",factor=0.5,patience=100,verbose=True)
 
@@ -38,14 +42,14 @@ if __name__ == '__main__':
   train_step = 0
   tset_step = 0
 
+  #写训练日志
+  shutil.rmtree("TrainLogs",ignore_errors=True)
   logWriter = SummaryWriter("TrainLogs")
 
   i = 0
-  totalLoss = 0
-  rightRate = 0
-  maxRightRate = 0
-  wait = 0    #当指定epoth后验证集准确率不再上升，则自动停止训练
-  maxwait = 5
+  minLoss = np.inf       #最小损失
+  wait = 0          #超时次数
+  maxwait = 5       #最大超时
   while(True):
     myModel.train(True)
     print(f"==========第{i+1}轮训练开始==========")
@@ -55,19 +59,20 @@ if __name__ == '__main__':
       imgs = imgs.to(device)
       targets = targets.to(device)
       outputs = myModel(imgs)
-      loss = lossFn(outputs,targets)
+      Avgloss = lossFn(outputs,targets)
       #写日志
-      logWriter.add_scalar("train Loss",loss,train_step)
+      logWriter.add_scalar("train Loss",Avgloss,train_step)
       #优化模型
       optimizer.zero_grad()
-      loss.backward()
+      Avgloss.backward()
       optimizer.step()
-      scheduler.step(loss)
+      scheduler.step(Avgloss)
       train_step+=1
 
     myModel.train(False)
     print("正在计算训练集准确度")
     trainTotalLoss = 0
+    trainAvgLoss = 0
     trainRightRate = 0
     trainRightCount = 0
     with torch.no_grad():
@@ -75,18 +80,21 @@ if __name__ == '__main__':
         imgs,targets = data
         imgs = imgs.to(device)
         targets = targets.to(device)
-        outputs = myModel(imgs)                     #跑训练集
-        result = torch.argmax(outputs,1)            #取最大值
+        outputs = myModel(imgs)                           #跑训练集
+        result = torch.argmax(outputs,1)                  #取最大值
         trainRightCount += torch.sum(result==targets)     #和标签比较
-        loss = lossFn(outputs,targets)              #代价函数
+        loss = lossFn(outputs,targets)                    #代价函数
         trainTotalLoss+=loss
       trainRightRate = trainRightCount/trainLen*100
-    print(f"训练损失:{trainTotalLoss/(trainLen/150)}，正确率：{trainRightRate:.2f}%")
+      trainAvgLoss = trainTotalLoss/(trainLen/batch)
+    print(f"训练损失:{trainAvgLoss:.3f}，正确率：{trainRightRate:.2f}%")
     logWriter.add_scalar("Train RT",trainRightRate,i)
 
 
     print("正在计算验证集准确度")
-
+    totalLoss = 0     #总损失
+    AvgLoss = 0       #平均损失
+    rightRate = 0     #正确率
     rightCount = 0
     with torch.no_grad():
       for data in verifyLoader:
@@ -95,28 +103,22 @@ if __name__ == '__main__':
         targets = targets.to(device)
         outputs = myModel(imgs)                     #跑验证集
         result = torch.argmax(outputs,1)            #取最大值
-        rightCount += torch.sum(result==targets)     #和标签比较
+        rightCount += torch.sum(result==targets)    #和标签比较
         loss = lossFn(outputs,targets)              #代价函数
         logWriter.add_images("Test data",imgs,train_step)
         totalLoss+=loss
       rightRate = rightCount/verifyLen*100
-    print(f"验证损失:{totalLoss/(verifyLen/150)}，正确率：{rightRate:.2f}%")
+      Avgloss = totalLoss/(verifyLen/batch)
+    print(f"验证损失:{Avgloss:.3f}，正确率：{rightRate:.2f}%")
     
     logWriter.add_scalar("Test RT",rightRate,i)
     #检测是否要停止训练
-    if(rightRate > maxRightRate):
-      maxRightRate = rightRate
+    if(Avgloss < minLoss):
+      minLoss = Avgloss
       wait = 0
-      torch.save(myModel.state_dict(),"myModel.pth")    #每训练一轮保存一次参数
+      torch.save(myModel.state_dict(),"Model.pth")    #每训练一轮保存一次参数
     else:
       wait+=1
       if(wait == maxwait):
         logWriter.close()
         exit(0)
-  """
-  shutil.rmtree("TrainLogs",ignore_errors=True)
-  logWriter = SummaryWriter("TrainLogs")
-  for batch in trainLoader:
-    logWriter.add_images("train images",batch[0],1)
-    break
-  logWriter.close()
